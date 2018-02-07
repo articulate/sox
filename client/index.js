d5eab30b1c83efed4e03351074e73dcb67d3f45f
Scott McCormack <flintinatux@users.noreply.github.com>
2018-02-07 14:05:03 -0500

    Add option for additional query params (#4)
    * Add optional query function
    
    * Also regen query on first connect
    
    * Pointfree bind (why not)
    
    * More docs

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
-const action = require('@articulate/ducks/lib/action')
-const Async  = require('crocks/Async')
-const cuid   = require('cuid')
-const curry  = require('ramda/src/curry')
-const evolve = require('ramda/src/evolve')
-const io     = require('socket.io-client')
-const pick   = require('ramda/src/pick')
-const URL    = require('url')
+const action  = require('@articulate/ducks/lib/action')
+const Async   = require('crocks/Async')
+const bind    = require('ramda/src/bind')
+const compose = require('ramda/src/compose')
+const cuid    = require('cuid')
+const curry   = require('ramda/src/curry')
+const evolve  = require('ramda/src/evolve')
+const io      = require('socket.io-client')
+const merge   = require('ramda/src/merge')
+const pick    = require('ramda/src/pick')
+const URL     = require('url')
 
 const debounce = require('./debounce')
 const throttle = require('./throttle')
@@ -15,18 +18,25 @@ const key = curry((type, payload) =>
   `${type}/${payload.id}`
 )
 
-const sox = ({ uri='' }={}) => {
-  const session = cuid()
+const sox = (args = {}) => {
+  const {
+    query = Function.prototype,
+    uri   = ''
+  } = args
+
+  const session = { session: cuid() }
   const url     = URL.parse(uri)
   const base    = URL.format(pick(['protocol', 'slashes', 'host'], url))
 
   const opts = {
     autoConnect: false,
     path: url.pathname,
-    query: { session }
+    query: merge(session, query())
   }
 
   const socket = io(base, opts)
+
+  const connect = bind(socket.connect, socket)
 
   const emit = curry((type, payload, done) =>
     socket.emit('action', action(type, payload), done)
@@ -40,6 +50,12 @@ const sox = ({ uri='' }={}) => {
     })
   )
 
+  const updateQuery = () =>
+    socket.io.opts.query = merge(session, query())
+
+  // connect : () -> Socket
+  socket.connect = compose(connect, updateQuery)
+
   // debounce : Number -> String -> a -> IO Async Action
   socket.debounce = curry((wait, type) =>
     debounce(wait, key(type), send(type))
@@ -48,12 +64,15 @@ const sox = ({ uri='' }={}) => {
   // send : String -> a -> Async Action
   socket.send = send
 
+  // session : String
   socket.session = session
 
   // debounce : Number -> String -> a -> IO Async Action
   socket.throttle = curry((wait, type) =>
     throttle(wait, key(type), send(type))
   )
+
+  socket.on('reconnect_attempt', updateQuery)
 
   return socket
 }
