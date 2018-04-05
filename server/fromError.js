ca276d1f10910a249fb34f0318572adb52fe44f5
Markus Greystone <markus.greystone@gmail.com>
2018-04-05 12:30:33 -0400

    Auto-convert Joi errors to Boom 400

--- server/fromError.js
+++ server/fromError.js
@@ -1,14 +1,21 @@
-const { boomify } = require('boom')
-const { path, pipe, prop, unless } = require('ramda')
+const Boom = require('boom')
+const { converge, identity, path, pipe, prop, unless, when } = require('ramda')
+
+const { boomify } = Boom
 
 const formatError = ({ data, message, error: name, statusCode: status }) =>
   ({ data, message, name, status })
 
+const joiError = pipe(
+  prop('details'),
+  converge(Boom.badRequest, [ prop('message'), identity ])
+)
+
 const fromError = pipe(
+  when(prop('isJoi'), joiError),
   unless(prop('isBoom'), boomify),
   path(['output', 'payload']),
   formatError
 )
 
 module.exports = fromError
-
